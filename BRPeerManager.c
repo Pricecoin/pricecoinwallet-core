5cb70b0715e04656832772399f203c97c92a56bd
Zach Price - PricecoinX <37559274+zachchan105@users.noreply.github.com>
2019-12-21 01:10:08 -0700

    checkpoints

--- BRPeerManager.c
+++ BRPeerManager.c
@@ -57,8 +57,6 @@ static const char *testnet_peers[] = {
 #if PRICECOINX_TESTNET
 
 static const struct { uint32_t height; const char *hash; uint32_t timestamp; uint32_t target; } checkpoint_array[] = {
-    {       0, "cee8f24feb7a64c8f07916976aa4855decac79b6741a8ec2e32e2747497ad2c9", 1481291250, 0x1e0ffff0 },
-    {   10000, "e84a668d907d002e013da912ac2d919e8613f97221ab0505a272b2e8dd6a71d8", 1510280787, 0x1e01aa34 }
 };
 
 static const char *dns_seeds[] = {
@@ -70,25 +68,10 @@ static const char *dns_seeds[] = {
 // blockchain checkpoints - these are also used as starting points for partial chain downloads, so they need to be at
 // difficulty transition boundaries in order to verify the block difficulty at the immediately following transition
 static const struct { uint32_t height; const char *hash; uint32_t timestamp; uint32_t target; } checkpoint_array[] = {
-    {      0, "4d96a915f49d40b1e5c2844d1ee2dccb90013a990ccea12c492d22110489f0c4", 1389311371, 0x1e0ffff0 },
-    {  24200, "d7ed819858011474c8b0cae4ad0b9bdbb745becc4c386bc22d1220cc5a4d1787", 1390701333, 0x1c130a52 },
-    {  65000, "9e673a69c35a423f736ab66f9a195d7c42f979847a729c0f3cef2c0b8b9d0289", 1396856613, 0x1c013819 },
-    {  84065, "a904170a5a98109b2909379d9bc03ef97a6b44d5dafbc9084b8699b0cba5aa98", 1399714612, 0x1c010fd5 },
-    { 228023, "15c94667a9e941359d2ee6527e2876db1b5e7510a5ded3885ca02e7e0f516b51", 1421367388, 0x1c02bba5 },
-    { 346992, "f1714fa4c7990f4b3d472eb22132891ccd3c7ad7208e2d1ab15bde68854fb0ee", 1439188880, 0x1c012830 },
-    { 347269, "fa1e592b7ea2aa97c5f20ccd7c40f3aaaeb31d1232c978847a79f28f83b6c22a", 1439231038, 0x1c01bf9c },
-    { 430000, "2f5703cf7b6f956b84fd49948cbf49dc164cfcb5a7b55903b1c4f53bc7851611", 1451641736, 0x1c00a632 },
-    { 516999, "572ed47da461743bcae526542053e7bc532de299345e4f51d77786f2870b7b28", 1464732960, 0x1c011edd }
+    {  0, uint256S("0xe96fe042f9a306b109b7276c47488fceafcbd98a4456676bcf42307087af30f2", 1389311371, 0x1e0ffff0}
 };
 
 static const char *dns_seeds[] = {
-    "useast1.vtconline.org.", // James Lovejoy
-    "vtc.gertjaap.org.", // Gert-Jaap Glasbergen
-    "seed.vtc.bryangoodson.org.", // Bryan Goodson
-    "seed.orderofthetaco.org.", // Eric (etang600)
-    "dnsseed.pknight.ca.", // pknight
-    "seed.alexturek.org.", // Alex Turek
-    "vertcoin.mbl.cash." // jk_14
 };
 
 #endif
