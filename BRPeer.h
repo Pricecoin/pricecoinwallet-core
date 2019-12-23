ca8c142cbd2fe6c08918afc9f2078ce3f383148b
Zach Price - PricecoinX <37559274+zachchan105@users.noreply.github.com>
2019-12-23 12:34:45 -0700

    Update BRPeer.h

--- BRPeer.h
+++ BRPeer.h
@@ -63,7 +63,7 @@ extern "C" {
 #define SERVICES_NODE_BCASH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
     
 #define BR_VERSION "175"
-#define USER_AGENT "/loaf-android:" BR_VERSION "/"
+#define USER_AGENT "/pricecoinx-android:" BR_VERSION "/"
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
 #define MSG_VERSION     "version"
